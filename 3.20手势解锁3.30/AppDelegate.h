f79e5acf9c0fa7d40328ed1c3316501e33d729b5
邓金明 <Jimmy@dengjinmingdemacbook-pro.local>
2016-03-30 19:41:54 +0800

    Initial Commit

---
+++ 3.20手势解锁3.30/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3.20手势解锁3.30
+//
+//  Created by 邓金明 on 16/3/30.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3.20手势解锁3.30/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3.20手势解锁3.30
+//
+//  Created by 邓金明 on 16/3/30.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
